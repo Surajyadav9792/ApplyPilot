a71189e19d6e157cfd7da913bd60b0bc55009f98
Surajyadav9792 <surajyadavmahadewa@gmail.com>
2026-07-20 16:00:00 +0530

    email sender utility code added

---
+++ server/utils/sendEmail.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+const sendEmail = async (options) => {
+    try {
+    if(!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
+        throw new Error("Email credentials are not set in environment variables");
+    }
+  const transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: options.to,
+    subject: options.subject,
+    text: options.text,
+    html: options.html || `<p>${options.text}</p>`, // Optional: If you want to send HTML content
+    attachments: options.attachments || [],
+  };
+
+  await transporter.sendMail(mailOptions);
+  console.log(`Email sent to ${options.to}`);
+} catch (error) {
+  console.error(`Error sending email to ${options.to}:`, error);
+  throw new Error("Email could not be sent");   
+ }
+};
+module.exports = sendEmail;
