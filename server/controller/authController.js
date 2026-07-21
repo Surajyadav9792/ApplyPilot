7a6bf93cb287c68950097df3cca14ffb082dea8c
Surajyadav9792 <surajyadavmahadewa@gmail.com>
2026-07-21 13:00:00 +0530

    auth controller boilerplate code

---
+++ server/controller/authController.js
@@ -0,0 +1,7 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+exports.register = async (req, res) => {
+  res.status(501).send("Not implemented yet");
+};
